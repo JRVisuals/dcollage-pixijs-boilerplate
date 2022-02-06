b786f4d65fe164b13b592e047788eb76c0db8adb
jrod-disco <jrod.disco@gmail.com>
2022-02-06 12:30:26 -0500

    add loader array to default contstants

--- src/components/library/preloader/index.ts
+++ src/components/library/preloader/index.ts
@@ -1,6 +1,6 @@
 import * as PIXI from 'pixi.js';
 import * as PIXISOUND from 'pixi-sound'; // Can we preload sounds here too?
-import { APP_VERSION } from '@src/constants';
+import { APP_VERSION, PRELOAD_ARRAY } from '@src/constants';
 export interface Preloader {
   init: (onAssetsLoadedCallback: () => void) => void;
   secondaryLoad: (onAssetsLoadedCallback: () => void) => void;
@@ -31,18 +31,10 @@ export const preloader = (props: PreloaderProps): Preloader => {
   const init = (onAssetsLoadedCallback: () => void): void => {
     // Preload Assets ------------------------------------------------
 
-    preloader
-      .add('main', './assets/example/sprites.json')
-      .add('ui', './assets/example/sprites-ui.json')
-
-      .add('Atari-16', './assets/font/Atari-16.fnt')
-      .add('Atari-32', './assets/font/Atari-32.fnt')
-
-      .add('FFFCorp-16', './assets/font/FFFCorp-16.fnt')
-      .add('FFFCorp-16-bold', './assets/font/FFFCorp-16-bold.fnt')
-
-      .add('FFFFuego-16', './assets/font/FFFFuego-16.fnt')
-      .add('FFFFuego-16-bold', './assets/font/FFFFuego-16-bold.fnt');
+    // Preload Array Set in Constants allows for any number of assets to be set to load
+    PRELOAD_ARRAY.forEach((e) => {
+      preloader.add(e.name, e.url);
+    });
 
     preloader.load(onAssetsLoadedCallback);
   };
--- src/constants/default.ts
+++ src/constants/default.ts
@@ -1,7 +1,7 @@
 // App
 
-// Ignoring this to suppress an error with __VERSION__ which will be rewritten with the version number on build.
 // @ts-ignore
+// Ignoring this to suppress an error with __VERSION__ which will be rewritten with the version number on build.
 export const APP_VERSION = __VERSION__; // <- populated by rollup replace
 // @ts-ignore
 export const DCO_VERSION = __DCOVERSION__; // <- populated by rollup replace
@@ -21,3 +21,17 @@ export const SFX_VOL_MULT = 0.5;
 
 // Environment
 export const TIME_LIMIT_SECONDS = 120;
+
+// Loader
+// Assets
+export const PRELOAD_ARRAY = [
+  { name: 'main', url: './assets/example/sprites.json' },
+  { name: 'ui', url: './assets/example/sprites-ui.json' },
+
+  { name: 'Atari-16', url: './assets/font/Atari-16.fnt' },
+  { name: 'Atari-32', url: './assets/font/Atari-32.fnt' },
+  { name: 'FFFCorp-16', url: './assets/font/FFFCorp-16.fnt' },
+  { name: 'FFFCorp-16-bold', url: './assets/font/FFFCorp-16-bold.fnt' },
+  { name: 'FFFFuego-16', url: './assets/font/FFFFuego-16.fnt' },
+  { name: 'FFFFuego-16-bold', url: './assets/font/FFFFuego-16-bold.fnt' },
+];
